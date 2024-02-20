c4ff6ba36f0a6ff55efc869dadc96cbbfd4cb7ee
Silmi Camilia <silmicml@gmail.com>
2024-02-20 14:10:30 +0700

    suda

---
+++ app/src/main/java/com/example/myrecyclerview/GridHeroHadapter.kt
@@ -0,0 +1,35 @@
+package com.example.myrecyclerview
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ImageView
+import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
+import com.bumptech.glide.request.RequestOptions
+
+
+class GridHeroHadapter(val listHeroes: ArrayList<Hero>) : RecyclerView.Adapter<GridHeroHadapter.GridViewHolder>() {
+    override fun onCreateViewHolder(viewGroup: ViewGroup, i: Int): GridViewHolder {
+        val view: View = LayoutInflater.from(viewGroup.context).inflate(R.layout.item_grid_president, viewGroup, false)
+        return GridViewHolder(view)
+    }
+
+
+    override fun onBindViewHolder(holder: GridViewHolder, position: Int) {
+        Glide.with(holder.itemView.context)
+            .load(listHeroes[position].photo)
+            .apply(RequestOptions().override(350, 550))
+            .into(holder.imgPhoto)
+    }
+
+    override fun getItemCount(): Int {
+        return listHeroes.size
+    }
+
+
+    inner class GridViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+        var imgPhoto: ImageView = itemView.findViewById(R.id.img_item_photo)
+    }
+}
+
